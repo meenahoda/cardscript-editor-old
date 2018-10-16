743a50b071f2fb3987a2f979e5d5e8735d9282a3
Meena.Hoda <meena.hoda@wmfs.net>
2018-10-16 09:51:25 +0100

    Number widget implementation

--- src/components/Number.vue
+++ src/components/Number.vue
@@ -1,18 +1,54 @@
 <template>
-  <div>Number</div>
+  <div>
+    <q-input
+      float-label="Heading"
+      v-model="data.attributes.heading"
+      dark
+      color="secondary"
+    />
+
+    <div class="row">
+      <div class="col q-pr-md">
+        <q-input
+          float-label="Default"
+          v-model="data.attributes.default"
+          dark
+          color="secondary"
+        />
+      </div>
+      <div class="col q-pr-md">
+        <q-input
+          float-label="Minimum"
+          v-model="data.attributes.minimum"
+          dark
+          color="secondary"
+        />
+      </div>
+      <div class="col">
+        <q-input
+          float-label="Maximum"
+          v-model="data.attributes.maximum"
+          dark
+          color="secondary"
+        />
+      </div>
+    </div>
+
+    <q-toggle
+      label="Mandatory"
+      v-model="data.attributes.mandatory"
+      dark
+      color="secondary"
+      left-label
+      class="q-mt-md"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'QScriptNumber',
-  data () {
-    return {
-      attributes: {
-        heading: '',
-        mandatory: false
-      }
-    }
-  }
+  props: [ 'data' ]
 }
 </script>
 
